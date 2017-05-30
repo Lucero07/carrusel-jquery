8ff29231085523c28c1e47aa2ad006e32fd01f3e
Lucero07 <ing.lucero.ramos@gmail.com>
2017-05-29 21:02:40 -0500

    final con modificaciones

--- js/main.js
+++ js/main.js
@@ -1,29 +1,43 @@
 $(document).ready(function () {
+
   $("#boton1").click(function () {
-      $("#flor1").css("display","inline-block");
-      $("#flor2").css("display","none");
-      $("#flor3").css("display","none");
-      $("#flor4").css("display","none");
-  });
-  $("#boton2").click(function () {
-      $("#flor2").css("display","inline-block");
-      $("#flor1").css("display","none");
-      $("#flor3").css("display","none");
-      $("#flor4").css("display","none");
-  });
-  $("#boton3").click(function () {
-      $("#flor3").css("display","inline-block");
-      $("#flor1").css("display","none");
-      $("#flor2").css("display","none");
-      $("#flor4").css("display","none");
-  });
-  $("#boton4").click(function () {
-      $("#flor4").css("display","inline-block");
-      $("#flor1").css("display","none");
-      $("#flor2").css("display","none");
-      $("#flor3").css("display","none");
+    $('.flores').css('display',"none");
+    $("#flor1").css("display","inline-block");
+
+  //     $("#flor1").css("display","inline-block");
+  //     $("#flor2").css("display","none");
+  //     $("#flor3").css("display","none");
+  //     $("#flor4").css("display","none");
+   });
+   $("#boton2").click(function () {
+     $('.flores').css('display',"none");
+     $("#flor2").css("display","inline-block");
+
+  //     $("#flor2").css("display","inline-block");
+  //     $("#flor1").css("display","none");
+  //     $("#flor3").css("display","none");
+  //     $("#flor4").css("display","none");
+   });
+   $("#boton3").click(function () {
+     $('.flores').css('display',"none");
+     $("#flor3").css("display","inline-block");
+
+  //     $("#flor3").css("display","inline-block");
+  //     $("#flor1").css("display","none");
+  //     $("#flor2").css("display","none");
+  //     $("#flor4").css("display","none");
+   });
+   $("#boton4").click(function () {
+     $('.flores').css('display',"none");
+     $("#flor4").css("display","inline-block");
+  //     $("#flor4").css("display","inline-block");
+  //     $("#flor1").css("display","none");
+  //     $("#flor2").css("display","none");
+  //     $("#flor3").css("display","none");
   });
 
+  //intervalo tiempo cambio img slide, 5 segundos, se llama a la función "avanzaSlide()"
+  setInterval(avanzaSlide,2000);
 
 
 });
@@ -31,36 +45,36 @@ $(document).ready(function () {
 
 
 
-//intervalo tiempo cambio img slide, 5 segundos, se llama a la función "avanzaSlide()"
- setInterval('avanzaSlide()',5000);
 
  //array con las clases para las diferentes imagenes
- var arrayImagenes = new Array(".flor1",".flor2",".flor3","flor4",".flor");
+ var arrayImagenes = new Array(".flor1",".flor2",".flor3",".flor4",".flor");
 
  //variable que nos permitirá saber qué imagen se está mostrando
- var contador = 0;
+var actual = 0, anterior = -1;
 
  //Muestre los elementos emparejados desvaneciéndolos a opaco.
  function mostrar(flor){
- 	$(flor).ready(function(){
-   		$(arrayImagenes[contador]).fadeIn(1000);
- 	});
+ // 	$(flor).ready(function(){
+      // console.log(this);
+   		$(flor).fadeIn(1000);
+ // 	});
  }
 
  //hace un efecto fadeOut para ocultar una imagen
  //Ocultar los elementos coincidentes por desvanecimiento de ellos a transparente.
  function ocultar(flor){
- 	$(flor).ready(function(){
-   		$(arrayImagenes[contador]).fadeOut(1000);
- 	});
+ // 	$(flor).ready(function(){
+   		$(flor).fadeOut();
+ // 	});
  }
 
  //función principal
  function avanzaSlide(){
          //se oculta la imagen actual
- 	ocultar(arrayImagenes[contador]);
-         //aumentamos el contador en una unidad
- 	contador = (contador +1) % 4;
+ 	ocultar(arrayImagenes[anterior]);
          //mostramos la nueva imagen
- 	mostrar(arrayImagenes[contador]);
+ 	mostrar(arrayImagenes[actual]);
+         //aumentamos el actual en una unidad
+  anterior = actual;
+ 	actual = (actual +1) % 4;
  }
