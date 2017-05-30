3d7a2a347351e5903217480515755650307a1804
Lucero07 <ing.lucero.ramos@gmail.com>
2017-05-29 21:46:31 -0500

    final

--- js/main.js
+++ js/main.js
@@ -3,42 +3,24 @@ $(document).ready(function () {
   $("#boton1").click(function () {
     $('.flores').css('display',"none");
     $("#flor1").css("display","inline-block");
-
-  //     $("#flor1").css("display","inline-block");
-  //     $("#flor2").css("display","none");
-  //     $("#flor3").css("display","none");
-  //     $("#flor4").css("display","none");
    });
    $("#boton2").click(function () {
      $('.flores').css('display',"none");
      $("#flor2").css("display","inline-block");
-
-  //     $("#flor2").css("display","inline-block");
-  //     $("#flor1").css("display","none");
-  //     $("#flor3").css("display","none");
-  //     $("#flor4").css("display","none");
    });
    $("#boton3").click(function () {
      $('.flores').css('display',"none");
      $("#flor3").css("display","inline-block");
-
-  //     $("#flor3").css("display","inline-block");
-  //     $("#flor1").css("display","none");
-  //     $("#flor2").css("display","none");
-  //     $("#flor4").css("display","none");
    });
+
    $("#boton4").click(function () {
      $('.flores').css('display',"none");
      $("#flor4").css("display","inline-block");
-  //     $("#flor4").css("display","inline-block");
-  //     $("#flor1").css("display","none");
-  //     $("#flor2").css("display","none");
-  //     $("#flor3").css("display","none");
+
   });
 
   //intervalo tiempo cambio img slide, 5 segundos, se llama a la función "avanzaSlide()"
-  setInterval(avanzaSlide,2000);
-
+  setInterval(avanzaSlide,5000);
 
 });
 
@@ -54,19 +36,14 @@ var actual = 0, anterior = -1;
 
  //Muestre los elementos emparejados desvaneciéndolos a opaco.
  function mostrar(flor){
- // 	$(flor).ready(function(){
-      // console.log(this);
    		$(flor).fadeIn(1000);
- // 	});
- }
+ };
 
  //hace un efecto fadeOut para ocultar una imagen
  //Ocultar los elementos coincidentes por desvanecimiento de ellos a transparente.
  function ocultar(flor){
- // 	$(flor).ready(function(){
    		$(flor).fadeOut();
- // 	});
- }
+ };
 
  //función principal
  function avanzaSlide(){
